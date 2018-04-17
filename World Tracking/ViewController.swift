49df31c25a98535db2c8d9454b4ab87fcf57c4b9
Yash Pahade <yash.aries.6@gmail.com>
2018-04-17 00:44:42 -0700

    Reset Tracking

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -32,5 +32,19 @@ class ViewController: UIViewController {
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
+    @IBAction func reset(_ sender: Any) {
+        self.restartSession()
+    }
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes{ (node, _) in
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+            
+    }
+    
+    
 }
 
