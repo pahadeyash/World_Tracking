6401ff0537d71f76b708e20fc0e8933e128201c5
Yash Pahade <yash.aries.6@gmail.com>
2018-04-16 23:31:02 -0700

    Positioning Nodes

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -24,6 +24,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0,0,0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    
 }
 
